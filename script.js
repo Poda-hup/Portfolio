80173eadd19d6c8837da668ec24c7b20894bb0ea
Abdelrahman Amin <abamin253@gmail.com>
2026-04-17 22:27:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,46 @@ const typed = new Typed('.multiple-text', {
     backDelay:1200,
     loop:true,
   });
+
+  const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+
+
+// تشغيل EmailJS
+(function(){
+    emailjs.init("uD24HVYjSf771B1td");
+})();
+
+// لما الفورم يتبعت
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_fymjkgm", "template_3c48o3l", this)
+    .then(function() {
+        alert("Message Sent Successfully ✅🔥");
+        document.getElementById("contact-form").reset();
+    }, function(error) {
+        alert("Failed ❌");
+        console.log(error);
+    });
+});
